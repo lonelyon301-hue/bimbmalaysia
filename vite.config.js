aafa0de37e89ed9f92baf42cf79bf1eaa19f611b
lonelyon301 <lonelyon3.01@gmail.com>
2026-06-17 22:52:51 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
-    const devServerUrl = env.VITE_DEV_SERVER_URL ? new URL(env.VITE_DEV_SERVER_URL) : new URL('https://subrepand-ashly-doubtlessly.ngrok-free.dev/');
+    const devServerUrl = env.VITE_DEV_SERVER_URL
+        ? new URL(env.VITE_DEV_SERVER_URL)
+        : new URL('http://localhost:5173');
 
     return {
         plugins: [
